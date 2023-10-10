6cfe69194f5957d8e32a8e4a1f2052b1cbbab219
Raphael Sonabend <raphael.sonabend@genomicsengland.co.uk>
2023-10-10 13:11:43 +0100

    fix theme submodule

---
+++ themes/up-business-theme/assets/js/shared/shared.js
@@ -0,0 +1,13 @@
+import "./_bootstrap";
+
+import "./_header";
+import "./_footer";
+
+import "../sections/hero";
+import "../sections/brands";
+import "../sections/content";
+import "../sections/services";
+import "../sections/cta";
+
+import "../sections/blog-list";
+import "../sections/blog-single";
